15772353332a6c12ef60ba74f88ffe7c8d894150
madafue <48069000+madafue@users.noreply.github.com>
2020-07-28 14:21:58 -0400

    the actual code

--- ShadowSeedFinder/Program.cs
+++ ShadowSeedFinder/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ShadowSeedFinder
 {
@@ -6,7 +6,45 @@ namespace ShadowSeedFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            Function();
+           
+        }
+
+        static void Function()
+        {
+
+            Console.WriteLine("Enter the seed to find the shadow of:");
+            string input = Console.ReadLine();
+            try
+            {
+                long seed = long.Parse(input);
+                long test = -7379792620528906219;
+                long shadow = test - seed;
+                Console.WriteLine("This is a test function. The seed you entered was  " + seed);
+                Console.WriteLine("The approximate shadow seed is: " + shadow);
+                Console.WriteLine("Do you want to enter another seed? [Yes/No]");
+                string response = Console.ReadLine();
+
+                if (response.Contains("y"))
+                {
+
+                    Main(null);
+
+                }
+                else
+                {
+
+                    Console.Write("Goodbye....");
+
+                }
+                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{input}' try again with a proper seed next time");
+            }
+
         }
     }
 }
